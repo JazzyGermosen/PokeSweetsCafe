aab5843d34035d289a4b0e2ff4f33f4ca84cffcc
Jazz <jazzygermosen@gmail.com>
2025-11-12 19:42:24 -0500

    finished the logic for the platter class and in the next commit i will attempt to replicate this same logic into my side, drinks and sweets class

--- src/main/java/com/pluralsight/models/Platter.java
+++ src/main/java/com/pluralsight/models/Platter.java
@@ -8,110 +8,48 @@ public class Platter extends MenuItems {
     // extra meat rice or veggies
     private boolean extraMeat;
     private boolean isSpicy;
-    private String name;
     private String meat;
     private String size;
-    private sideType type;
+
     // ^^ all the things a plate would have
-
-    public enum sideType {
-        PREMIUM,
-        SUSHI,
-        EGG,
-        REGULAR
-    }
-
+    // ctrl left click is cool
     // list from sides class
     private ArrayList<Side> sides = new ArrayList<>();
-    private ArrayList<sideType> sideTypes = new ArrayList<>();
-
-    public Platter(double totalPrice, ArrayList<sideType> sideTypes, ArrayList<Side> sides, sideType type, String size, String meat, String name, boolean isSpicy, boolean extraMeat) {
-        super(totalPrice);
-        this.sideTypes = sideTypes;
-        this.sides = sides;
-        this.type = type;
-        this.size = size;
-        this.meat = meat;
-        this.name = name;
-        this.isSpicy = isSpicy;
-        this.extraMeat = extraMeat;
-    }
-
-    // focus on everything that comes on the platter
-    // anything that can be ordered seperately will be its own class
-    // create a list that will store the types
-    // shift one of the options to make it a premium options
-    // can have an extra meet as a way to add "toppings that would cost more"
-    // this method will add a side to the order
-    // am unsure if this needs to be in the order class
-    public void addSide(SideType sides, Side SideType){
-        this.sides.add(SideType);
-        // trying to call from the side class to use in here
-    }
 
 
-    public double findTotalPrice(String platterPrice){
-        double basePrice = switch (platterPrice.toLowerCase()){
+    public Platter( String size) {
+        //this is the platter constructor
+        // taking the size of the platter to calculate the base price of the platter to be passed into the super construtor
+        super( switch (size.toLowerCase()){
             case "kids" -> 6.50;
             case "half" -> 8.00;
             case "full" -> 10.50;
             default -> 0;
-        };;
-        //^ i dont understand why this is needed will ask later
-        // the for loop will itterate through the list of sides
-        // inside the loop it will check each sides type and then calculate it based off of that
+        });
+
+    }
+
+    public double getPrice(){
+        // base price of platter
+        // add the price of all the sides to the base price
+        // add fee for extra meat
+        // return total
+        //iterate through the sides array
+        double currentTotal = this.getTotalPrice();
         for(Side side : sides){
-            basePrice += side.getTotalPrice();
+            currentTotal += side.getTotalPrice();
+
         }
-        return getTotalPrice();
+        if (extraMeat){
+            currentTotal += 2.00;
+        }
+        return currentTotal;
     }
 
 
-    public boolean isExtraMeat() {
-        return extraMeat;
+    public void addSide(Side s){
+        this.sides.add(s);
+        // trying to call from the side class to use in here
     }
 
-    public void setExtraMeat(boolean extraMeat) {
-        this.extraMeat = extraMeat;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMeat() {
-        return meat;
-    }
-
-    public void setMeat(String meat) {
-        this.meat = meat;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    public sideType getType() {
-        return type;
-    }
-
-    public void setType(sideType type) {
-        this.type = type;
-    }
-
-    public ArrayList<Side> getSides() {
-        return sides;
-    }
-
-    public void setSides(ArrayList<Side> sides) {
-        this.sides = sides;
-    }
 }
--- src/main/java/com/pluralsight/models/Side.java
+++ src/main/java/com/pluralsight/models/Side.java
@@ -4,55 +4,30 @@ public class Side extends MenuItems  {
     // dont know if this will be extending or implimenting to the parent function because toppings comes from sandwhich
     // creating the objects that will be what the topings are
    // since sides would be something thats added and seperate i have to figure out a different logic flow for this class
-    private sideType type;
-    private String name;
-
-    public Side(double totalPrice, sideType type, String name) {
-        super(totalPrice);
-        this.type = type;
-        this.name = name;
-    }
-
-    // create an enum to describe the type of side that it is ( reg , premium ect)
-    // so that when we go to the platter we will have an easier way to tell what it is
-    // enum sidetype ( list of words from sushi egg and regular )
-    // when you make a side object it would have 2 properties
-    // the name of the side and the type of the side and it has to be from the enum that we defined
-    // private toppingtype type;
-    // its going to itterate through the list of side and check the number of sides
-    // once it does know the number of sides it will do the calculations based off of that
+    private SideType type;
+    private String size;
 
 
-    public double priceForSide(String sidePrice){
-        return switch (sidePrice.toLowerCase()){
+    public Side(SideType type, String size) {
+        super(switch (size.toLowerCase()){
             case "kids" -> 3.00;
             case "full" -> 5.00;
             default -> 0;
-        };
-        // method for finding price of side if it is an extra order ??
+        });
     }
+
     @Override
     public String toString(){
         return type.toString();
     }
 
-        public enum sideType {
-
-        }
-
-    public sideType getType() {
+    public SideType getType() {
         return type;
     }
 
-    public void setType(sideType type) {
+    public void setType(SideType type) {
         this.type = type;
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 }
